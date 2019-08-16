abdc47246ce8cc520e2da3094fe79fd184073268
Jezy Jurgelianecas <jezyj@interxion.com>
2019-08-16 13:00:18 +0100

    Disabled lint checking for console

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,9 @@ module.exports = {
   extends: ["@open-wc/eslint-config", "eslint-config-prettier"].map(
     require.resolve
   ),
-  rules: { "class-methods-use-this": 0, "linebreak-style": 0 }
+  rules: {
+    "class-methods-use-this": 0,
+    "linebreak-style": 0,
+    "no-console": "off"
+  }
 };
